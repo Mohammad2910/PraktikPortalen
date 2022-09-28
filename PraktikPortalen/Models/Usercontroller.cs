a7d99762c9b2943fa902f0f9b9e0655376ef90b2
Mohammad Uddin <s184174@student.dtu.dk>
2022-09-28 20:04:20 +0200

    Deleted irrelevant files to give proper overview

--- PraktikPortalen/Models/Usercontroller.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace PraktikPortalen.Models
-{
-    public class Usercontroller
-    {
-        public int number { get; set; }
-    }
-}
--- PraktikPortalen/Program.cs
+++ PraktikPortalen/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Test}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
