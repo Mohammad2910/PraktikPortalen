258e7dbad7c0cdc99355b17c282af63d7347417b
Mohammad Uddin <s184174@student.dtu.dk>
2022-12-20 09:59:12 +0100

    a

--- PraktikPortalen/Controllers/InternshipsController.cs
+++ PraktikPortalen/Controllers/InternshipsController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
 using PraktikPortalen.Models;
+using System.Net;
 
 namespace PraktikPortalen.Controllers
 {
@@ -41,6 +43,41 @@ namespace PraktikPortalen.Controllers
                 //}
             }
             return View(internships);
+        }
+
+        public IActionResult New(InternshipsModel internship)
+        {
+            System.Diagnostics.Debug.WriteLine(internship.InternshipName);
+            System.Diagnostics.Debug.WriteLine(internship.InternshipCompany);
+            internship.Status = "submitted";
+            internship.user_id = user.id;
+            System.Diagnostics.Debug.WriteLine(internship.Status);
+            System.Diagnostics.Debug.WriteLine(internship.user_id);
+            System.Diagnostics.Debug.WriteLine(internship.DTUSupervisor_id.ToString());
+            System.Diagnostics.Debug.WriteLine(internship.CompanySupervisor_id.ToString());
+            System.Diagnostics.Debug.WriteLine("!!!!!!!!!!!!!!!!!!");
+            if (!string.IsNullOrEmpty(internship.InternshipName) && !string.IsNullOrEmpty(internship.DTUSupervisor_id.ToString()))
+            {
+                HttpClientHandler clientHandler = new HttpClientHandler();
+                clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+                using (var client = new HttpClient(clientHandler))
+                {
+                    client.BaseAddress = new Uri("https://localhost:5287/api/internships");
+                    var postTask = client.PostAsJsonAsync<InternshipsModel>("", internship);
+                    postTask.Wait();
+                    var result = postTask.Result;
+                    System.Diagnostics.Debug.WriteLine(result.StatusCode);
+                    if (result.IsSuccessStatusCode)
+                    {
+                        System.Diagnostics.Debug.WriteLine("SUCCEEEEESSS!!!!!!!!!!!!!!!!!!");
+                        // var readTask = result.Content.ReadAsAsync<Intern>();
+                        // session.SetString("User", JsonConvert.SerializeObject(readTask.Result));
+                        return RedirectToAction("Internships");
+                    }
+                }
+            }
+
+            return View();
         }
     }
 }
